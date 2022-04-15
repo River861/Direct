18c6d00729cb388778675196c017de1f033d4484
jcshen <jcshen@node-0.ddc-3rpc.scalablerpc-pg0.utah.cloudlab.us>
2022-04-14 22:35:13 -0600

    debug

--- libhrd/hrd_conn.c
+++ libhrd/hrd_conn.c
@@ -497,6 +497,9 @@ void hrd_connect_qp(struct hrd_ctrl_blk* cb, int n,
   // TODO 从remote_qp_attr中拿gid
   if(USE_ROCE) {
     memcpy(&conn_attr.ah_attr.grh.dgid, remote_qp_attr->gid, 16);
+		uint8_t *p = remote_qp_attr->gid;
+		fprintf(stdout, "Remote GID =%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x\n ",p[0],
+				  p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9], p[10], p[11], p[12], p[13], p[14], p[15]);
   }
 
   if (ibv_modify_qp(cb->conn_qp[n], &conn_attr, rtr_flags)) {   // TODO
@@ -608,7 +611,7 @@ void hrd_publish_conn_qp(struct hrd_ctrl_blk* cb, int n, const char* qp_name) {
   memset(qp_attr.gid, 0, 16);
   if(USE_ROCE) {
     int gid_idx = 0;
-    int rc = ibv_query_gid(cb->ctx, cb->dev_port_id, gid_idx, &my_gid);
+    int rc = ibv_query_gid(cb->conn_qp[n]->context, cb->dev_port_id, gid_idx, &my_gid);
     if (rc) {
       fprintf(stderr, "could not get gid for port %d, index %d\n", cb->dev_port_id, gid_idx);
     }
