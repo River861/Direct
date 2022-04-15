031632951916314b2e54bd5c0de7c587064702b0
xuchuan <xuchuan@node-0.jcshen-123759.scalablerpc-pg0.utah.cloudlab.us>
2022-04-15 16:27:43 -0600

    test

--- herd/main.h
+++ herd/main.h
@@ -40,7 +40,7 @@
 #define MAX_SERVER_PORTS 4
 #define NUM_WORKERS 4
 #define NUM_CLIENTS 8  // 32
-#define NUM_CLIENT_NODE 7
+#define NUM_CLIENT_NODE 6
 #define WORKLOAD_DIR "upd-workloads"
 #define WORKLOAD_ID "upd90"
 #define NUM_MEMORY 2  // MICA_NUM_MEMORY
